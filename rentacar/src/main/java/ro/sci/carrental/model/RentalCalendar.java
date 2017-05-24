1784fdc4a0e5b8da4a3a4cad067fca24c16b12c8
Dragan Tudor <tudordragan27@gmail.com>
2017-05-25 00:31:16 +0300

    hw rentacar git first commit

---
+++ rentacar/src/main/java/ro/sci/carrental/model/RentalCalendar.java
@@ -0,0 +1,54 @@
+package ro.sci.carrental.model;
+
+/**
+ * Created by tudor on 24.05.2017.
+ */
+public class RentalCalendar{
+    private boolean drivingLicence;
+    private double pickupTime;
+    private double returnTime;
+    private double price;
+
+
+    public RentalCalendar(boolean drivingLicence, double price, double pickupTime) {
+        this.drivingLicence = drivingLicence;
+        this.price = price;
+        this.pickupTime = pickupTime;
+    }
+
+    public double getReturnTime() {
+        return returnTime;
+    }
+
+    public RentalCalendar() {
+    }
+
+    public void setReturnTime(double returnTime) {
+        this.returnTime = returnTime;
+    }
+
+    public boolean isDrivingLicence() {
+        return drivingLicence;
+    }
+
+    public void setDrivingLicence(boolean drivingLicence) {
+        this.drivingLicence = drivingLicence;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPickupTime() {
+        return pickupTime;
+    }
+
+    public void setPickupTime(double pickupTime) {
+        this.pickupTime = pickupTime;
+    }
+
+}
